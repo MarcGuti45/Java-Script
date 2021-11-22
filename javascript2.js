6c98651d757c382e386b15908295c018724398ed
MarcGuti45 <marc.gutierrez@institutvidreres.cat>
2021-11-22 19:28:58 +0100

    Exercici7

--- javascript2.js
+++ javascript2.js
@@ -1,6 +1,10 @@
-const BigInt = 5678;
+/* const BigInt = 5678;
 var num = 1234;
 console.log(num + BigInt)
 console.log(num - BigInt)
 console.log(num * BigInt)
-console.log(num / BigInt)
+console.log(num / BigInt) */
+
+var catet1 = 4
+var catet2 = 4
+console.log(catet1*catet1 + catet2*catet2)
