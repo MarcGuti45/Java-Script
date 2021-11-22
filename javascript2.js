5176afeafd09d187864505a500fc7654bf843a9b
MarcGuti45 <marc.gutierrez@institutvidreres.cat>
2021-11-22 19:38:32 +0100

    Exercici8

--- javascript2.js
+++ javascript2.js
@@ -1,10 +1,20 @@
-/* const BigInt = 5678;
+
+// Exercici 6
+/* var BigInt = 5678n;
 var num = 1234;
 console.log(num + BigInt)
 console.log(num - BigInt)
 console.log(num * BigInt)
 console.log(num / BigInt) */
 
-var catet1 = 4
+
+// Exercici 7
+/* var catet1 = 4
 var catet2 = 4
-console.log(catet1*catet1 + catet2*catet2)
+console.log(catet1*catet1 + catet2*catet2) */
+
+
+// Exercici 8 
+var num = 33
+var string = 'Hola que tal'
+console.log(num/string) 
