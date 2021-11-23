5ee3137d52df4dfc3e2e3e4554be38d63ba59042
MarcGuti45 <marc.gutierrez@institutvidreres.cat>
2021-11-23 08:35:58 +0100

    Exercici 1 Funcions

--- javascript2.js
+++ javascript2.js
@@ -1,5 +1,5 @@
 
-// Exercici 6
+// Exercici 6 Variables
 /* var BigInt = 5678n;
 var num = 1234;
 console.log(num + BigInt)
@@ -8,46 +8,46 @@ console.log(num * BigInt)
 console.log(num / BigInt) */
 
 
-// Exercici 7
+// Exercici 7 Variables
 /* var catet1 = 4
 var catet2 = 4
 console.log(catet1*catet1 + catet2*catet2) */
 
 
-// Exercici 8 
+// Exercici 8 Variables
 /* var num = 33
 var string = 'Hola que tal'
 console.log(num/string) */
 
 
-//Exercici 9
+//Exercici 9 Variables
 /* var nom = 'Marc Gutiérrez Luna'
 var cognom = nom.slice(4,13)
 console.log(cognom.length) */
 
 
-// Exercici 10
+// Exercici 10 Variables
 /* var nom = 'Marc Gutiérrez Luna'
 var canvi = nom.replace('u', '')
 console.log(canvi) */
 
 
-// Exercici 11
+// Exercici 11 Variables
 /* var variable1 = undefined
 var variable2 = null
 console.log(variable1 + variable2) */
 
 
-// Exercici 12
+// Exercici 12 Variables
 /* console.log(String(true))
 console.log(String + true) */
 
 
-// Exercici 13
+// Exercici 13 Variables
 /* console.log(Boolean('Hey com anem'))
 console.log(true || 'hola bon dia') */
 
 
-// Exercici 14
-console.log(Number(true))
-console.log(33 != true)
+// Exercici 14 Variables
+/* console.log(Number(true))
+console.log(33 != true) */
---
+++ javascript3.js
@@ -0,0 +1,6 @@
+
+// Exercici 1 Funcions
+function nom(nom, cognom){
+    console.log(nom + cognom)
+}
+nom('Marc', ' Gutiérrez')
