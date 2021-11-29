6eeb7a859c4d6af72f5a1d58560eafc69f74bb62
MarcGuti45 <marc.gutierrez@institutvidreres.cat>
2021-11-29 09:03:22 +0100

    Exercici 2 Lógica

--- jslogica.js
+++ jslogica.js
@@ -1,3 +1,5 @@
+// Exercici 1 Lógica
+/*
 var numero = 49
 
 if ((numero % 3) == 0){
@@ -9,3 +11,18 @@ else if ((numero % 7) == 0){
 else{
     console.log("El numero no es multiple ni de 3 ni de 7")
 }
+*/
+
+
+// Exercici 2 Lógica
+var x = 50
+var y = 9
+
+
+var rangx = x >= 10 && x <= 50 
+? console.log("El numero " , x , " esta dins del rang")
+: console.log("El numero " , x , " no esta dins del rang")
+
+var rangy = y >= 10 && x <= 50 
+? console.log("El numero " , y , " esta dins del rang")
+: console.log("El numero " , y , " no esta dins del rang")
