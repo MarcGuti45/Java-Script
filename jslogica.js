5dd72364778d13a591368be6672a0cb57b52a42c
MarcGuti45 <marc.gutierrez@institutvidreres.cat>
2021-11-29 08:30:14 +0100

    Exercici 1 Logica

---
+++ jslogica.js
@@ -0,0 +1,11 @@
+var numero = 49
+
+if ((numero % 3) == 0){
+    console.log("El numero es multiple de 3")
+}
+else if ((numero % 7) == 0){
+    console.log("El numero es multiple de 7")
+}
+else{
+    console.log("El numero no es multiple ni de 3 ni de 7")
+}
