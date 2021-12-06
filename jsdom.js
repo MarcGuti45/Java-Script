83eae59265796e517036cdcb804756431c5062e4
MarcGuti45 <marc.gutierrez@institutvidreres.cat>
2021-12-06 18:50:24 +0100

    Exercici 3 DOM

--- jsdom.js
+++ jsdom.js
@@ -6,4 +6,14 @@ hola.innerHTML = hola.innerHTML.toUpperCase();
 
 
 // Exercici 2 DOM 
+/*
 document.getElementById("url").textContent = document.URL;
+*/
+
+
+// Exercici 3 DOM 
+function colorGroc(){
+    document.getElementById("hola").style.color = "yellow";
+}
+
+colorGroc()
