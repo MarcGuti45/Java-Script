0ad454d90a5ade5ec6c7c1b68d2562e88c6f1ccb
MarcGuti45 <marc.gutierrez@institutvidreres.cat>
2021-12-06 18:29:46 +0100

    Exercici 2 DOM

--- jsdom.js
+++ jsdom.js
@@ -1,3 +1,9 @@
 // Exercici 1 DOM
+/*
 let hola = document.getElementById("hola");
 hola.innerHTML = hola.innerHTML.toUpperCase();
+*/
+
+
+// Exercici 2 DOM 
+document.getElementById("url").textContent = document.URL;
